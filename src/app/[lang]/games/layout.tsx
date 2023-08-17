d6fac8580f63e2f42162191cc814d68bb1d55904
Ernest <ealgenv@gmail.com>
2023-08-18 00:05:18 +0300

    .

---
+++ src/app/[lang]/games/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Games | Byster',
+}
+
+
+export default function GamesLayout({
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
